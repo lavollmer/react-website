4f2b30437989bd849b60e7e2aa0f308f8317789a
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-19 15:11:16 -0600

    adding comments to sidenav functionality

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
 function Sidenav() {
+  // state variable = nav, setNav updates the function of nav variable, initial value of nav is set to false
+  // when handleNav is called, it will update the function of nav to the opposite of its current value
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
@@ -12,7 +14,11 @@ function Sidenav() {
       {/* absolute means positioned to its closest relative */}
       {/* top offset to 4 units, right offset of 4 units */}
       {/* Z-index determines stack order of elements thus element will appear above others */}
-      <AiOutlineMenu className="absolute top-4 right-4 z-[99]" />
+      {/* anything above medium breakpoint is hidden */}
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
     </div>
   );
 }
