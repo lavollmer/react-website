bf69a951a3ae50528c53d023aa8709f7c202b1e9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-19 15:18:21 -0600

    styling Home navbar section

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -8,18 +8,31 @@ function Sidenav() {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
+    // testing => every time click then will log state changed in console
+    console.log("state changed");
   };
   return (
-    <div>
-      {/* absolute means positioned to its closest relative */}
-      {/* top offset to 4 units, right offset of 4 units */}
-      {/* Z-index determines stack order of elements thus element will appear above others */}
-      {/* anything above medium breakpoint is hidden */}
-      <AiOutlineMenu
-        onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] md:hidden"
-      />
-    </div>
+    <>
+      <div>
+        {/* absolute means positioned to its closest relative */}
+        {/* top offset to 4 units, right offset of 4 units */}
+        {/* Z-index determines stack order of elements thus element will appear above others */}
+        {/* anything above medium breakpoint is hidden */}
+        <AiOutlineMenu
+          onClick={handleNav}
+          className="absolute top-4 right-4 z-[99] md:hidden"
+        />
+        {nav ? (
+          <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+            <a href="#main"></a>
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </>
   );
 }
 
