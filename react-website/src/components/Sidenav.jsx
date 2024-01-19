7a3849f10aa332dc3111b5280b2100523d71f2ab
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-19 15:28:04 -0600

    adding comments to navbar styling home section

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -22,12 +22,16 @@ function Sidenav() {
           onClick={handleNav}
           className="absolute top-4 right-4 z-[99] md:hidden"
         />
+        {/* if nav is truthy will then display the following below */}
         {nav ? (
+          // fixed position container that covers entire screen, has semi-transparent white background, flex container with contents that are vertically aligned and center, with a z index of 20 giving it's stacking
           <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+            {/* link to main with styling of flex container, centered items, shadow is gray, hover */}
             <a
               href="#main"
               className="w-[75%} flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             ></a>
+            {/* AiOutlineHome icon from React icons with a size of 20 pixels */}
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </div>
