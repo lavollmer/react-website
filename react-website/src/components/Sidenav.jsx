09038eda4229777267597cca58bdbb4536dfcf64
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-19 15:06:30 -0600

    adding commenting of styling to code

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+// import AiOutlineMenu which is a hamburger style icon
 import { AiOutlineMenu } from "react-icons/ai";
 
 function Sidenav() {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div>
-      <AiOutlineMenu />
+      {/* absolute means positioned to its closest relative */}
+      {/* top offset to 4 units, right offset of 4 units */}
+      {/* Z-index determines stack order of elements thus element will appear above others */}
+      <AiOutlineMenu className="absolute top-4 right-4 z-[99]" />
     </div>
   );
 }
