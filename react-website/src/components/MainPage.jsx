ff2d3d1316fea56f57c42907b8d9d4e2b417893b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-20 11:54:08 -0600

    finalizing styling of main page with font

--- react-website/src/components/MainPage.jsx
+++ react-website/src/components/MainPage.jsx
@@ -10,9 +10,11 @@ function MainPage() {
         src="https://images.unsplash.com/photo-1705600196560-9e58cc3e18f4?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       ></img>
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div>
-          <h1>I'm Laura</h1>
-          <h2>
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Laura
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             I'm a
             <TypeAnimation
               sequence={[
