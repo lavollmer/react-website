2e5a64b5e223d5faee275d8b6c1913bc3bb25f5b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-20 11:35:48 -0600

    connected main page to app.jsx and added background image

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Sidenav from "./components/Sidenav";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
     <>
       <div>
         <Sidenav />
+        <MainPage />
       </div>
     </>
   );
---
+++ react-website/src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div id="MainPage">
+      <img
+        className=""
+        src="https://images.unsplash.com/photo-1705600196560-9e58cc3e18f4?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ></img>
+    </div>
+  );
+}
+
+export default MainPage;
