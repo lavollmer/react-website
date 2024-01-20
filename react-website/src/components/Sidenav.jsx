58082c91a2b3d6c0359a7aaabb91d45c7120e4a9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-20 11:26:29 -0600

    finished navbars

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -43,7 +43,7 @@ function Sidenav() {
               <span className="pl-4">Home</span>
             </a>
             <a
-              href="#main"
+              href="#work"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
               {/* GrProjects icon from React icons with a size of 20 pixels */}
@@ -51,7 +51,7 @@ function Sidenav() {
               <span className="pl-4">Work</span>
             </a>
             <a
-              href="#main"
+              href="#projects"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
               {/* AiOutlineProject icon from React icons with a size of 20 pixels */}
@@ -67,7 +67,7 @@ function Sidenav() {
               <span className="pl-4">Resume</span>
             </a>
             <a
-              href="#main"
+              href="#contact"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
               {/* AiOutlineMail icon from React icons with a size of 20 pixels */}
@@ -87,13 +87,13 @@ function Sidenav() {
               <AiOutlineHome size={20} />
             </a>
             <a
-              href="#main"
+              href="#work"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <GrProjects size={20} />
             </a>
             <a
-              href="#main"
+              href="#projects"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <AiOutlineProject size={20} />
@@ -105,7 +105,7 @@ function Sidenav() {
               <BsPerson size={20} />
             </a>
             <a
-              href="#main"
+              href="#contact"
               className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             >
               <AiOutlineMail size={20} />
