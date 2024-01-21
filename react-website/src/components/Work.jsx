6fdc8b8b01d251a5e1e7107709a78d41efed43f7
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-21 13:15:17 -0600

    setting up Work Section components in React

---
+++ react-website/src/components/Work.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { WorkItem } from "./WorkItem";
+
+//array of objects of data
+const data = [
+  {
+    year: 2023,
+    title: "Floral Designer",
+    duration: "9 months",
+    details: "lorem ipsum",
+  },
+  {
+    year: 2020,
+    title: "Content Manager",
+    duration: "1.5 years",
+    details: "lorem ipsum",
+  },
+  {
+    year: 2017,
+    title: "Clinical Pharmacy Specialist",
+    duration: "1.5 years",
+    details: "lorem ipsum",
+  },
+];
+const Work = () => {
+  return <div>Work</div>;
+};
+
+export default Work;
