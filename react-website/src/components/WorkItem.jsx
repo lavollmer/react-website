4658bedfd6a71147a6ba64c38f9247e254670b53
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-21 13:34:31 -0600

    adding workItem component code

--- react-website/src/components/WorkItem.jsx
+++ react-website/src/components/WorkItem.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export const WorkItem = () => {
-  return <div>WorkItem</div>;
+const WorkItem = ({ year, title, duration, details }) => {
+  return (
+    <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
+      <li>
+        <div />
+        <p>
+          <span>{year}</span>
+          <span>{title}</span>
+          <span>{duration}</span>
+        </p>
+        <p>{details}</p>
+      </li>
+    </ol>
+  );
 };
+
+export default WorkItem;
