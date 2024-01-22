723dc6799aeea9c412e55f159162a163fc5794f1
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-21 23:59:16 -0600

    adding shading to project components when hover

--- react-website/src/components/ProjectItem.jsx
+++ react-website/src/components/ProjectItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const ProjectItem = () => {
-  return <div>ProjectItem</div>;
+const ProjectItem = ({ img, title }) => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
+      <img
+        src={img}
+        alt="/"
+        className="rounded-xl group hover:opacity-10"
+      ></img>
+      <div>
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
