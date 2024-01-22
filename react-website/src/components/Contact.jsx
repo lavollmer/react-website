82e1d9448951288940fdc3c2f3cf5a3613d94c7d
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 10:22:53 -0600

    added name, phone number, email, subject and message sections

--- react-website/src/components/Contact.jsx
+++ react-website/src/components/Contact.jsx
@@ -6,7 +6,30 @@ const Contact = () => {
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form action="" method="POST" encType="multipart/form-data"></form>
+      <form action="" method="POST" encType="multipart/form-data">
+        <div>
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label>Phone Number</label>
+            <input type="text" name="phone" />
+          </div>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Subject</label>
+          <input type="text" name="subject" />
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea rows="10" name="message"></textarea>
+        </div>
+      </form>
     </div>
   );
 };
