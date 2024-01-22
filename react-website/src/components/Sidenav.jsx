8ffb0e1f372fdc28f9e05f70f9d871edd8bf440b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 10:35:08 -0600

    finalized navigation of sidebar for closing when clicked

--- react-website/src/components/Sidenav.jsx
+++ react-website/src/components/Sidenav.jsx
@@ -35,6 +35,7 @@ function Sidenav() {
           <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
             {/* link to main with styling of flex container, centered items, shadow is gray, hover */}
             <a
+              onClick={handleNav}
               href="#main"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
@@ -43,6 +44,7 @@ function Sidenav() {
               <span className="pl-4">Home</span>
             </a>
             <a
+              onClick={handleNav}
               href="#work"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
@@ -59,6 +61,7 @@ function Sidenav() {
               <span className="pl-4">Projects</span>
             </a>
             <a
+              onClick={handleNav}
               href="#main"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
@@ -67,6 +70,7 @@ function Sidenav() {
               <span className="pl-4">Resume</span>
             </a>
             <a
+              onClick={handleNav}
               href="#contact"
               className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
             >
