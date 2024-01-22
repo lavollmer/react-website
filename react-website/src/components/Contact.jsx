00d8d61a72ce78049cb902943a3f453d9f672611
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 10:31:36 -0600

    finalized styling of contact form with tailwindCSS

--- react-website/src/components/Contact.jsx
+++ react-website/src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
             />
           </div>
           <div className="flex flex-col">
-            <label className="uppercase text-sm py-2">Phone Number</label>
+            <label className="uppercase text-sm py-2">Phone</label>
             <input
               className="border-2 rounded-lg p-3 flex border-gray-300"
               type="text"
@@ -33,14 +33,25 @@ const Contact = () => {
             name="email"
           />
         </div>
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Subject</label>
-          <input type="text" name="subject" />
+          <input
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="text"
+            name="subject"
+          />
         </div>
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Message</label>
-          <textarea rows="10" name="message"></textarea>
+          <textarea
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            rows="10"
+            name="message"
+          ></textarea>
         </div>
+        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Send Message
+        </button>
       </form>
     </div>
   );
