80964812d271b291eec065fb0c7c4662b3273467
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 10:18:34 -0600

    setting up contact form

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -3,6 +3,7 @@ import Sidenav from "./components/Sidenav";
 import MainPage from "./components/MainPage";
 import Work from "./components/Work";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <MainPage />
         <Work />
         <Projects />
+        <Contact />
       </div>
     </>
   );
